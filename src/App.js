bf53d3b2de552eb89a7247389ec634795100e744
syliow <shanyiliow1107@gmail.com>
2021-09-17 07:04:02 +0900

    Fix position

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <div className="container profile">
+      <div className="container profile mt-5">
         <AboutMe />
         <MyButton />
       </div>
